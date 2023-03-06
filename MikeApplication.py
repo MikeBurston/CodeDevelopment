0e346bc595161fed5e3dbca9e9a4b84ba9cb7ab9
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 18:41:02 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -3,6 +3,6 @@ import pandas
 
 streamlit.text('Hello World')
 
-my_fruit_list = pandas.read_csv("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=gWioEyTJ4azAEr6PQm4slHxqU2CzzBln9JmoCf93")
+my_fruit_list = pandas.read_txt("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=gWioEyTJ4azAEr6PQm4slHxqU2CzzBln9JmoCf93")
 
 streamlit.dataframe(my_fruit_list)
