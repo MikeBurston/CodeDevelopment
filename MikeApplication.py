fcee8c75e92c208c787ace22183f93a964a1e32d
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 19:05:15 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -6,10 +6,3 @@ streamlit.text('Hello World')
 
 snowflake-connector-python
 
-conn = snowflake.connector.connect(
-            user="BIGMIKE2023",
-            password="Army501a!",
-            account="su30921.ca-central-1.aws",
-            warehouse="pc_rivery_wh",
-            database="pc_rivery_db",
-            schema="public")
