a0e14e750f35c3aac53224e8bac9dbe8c3349a71
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 18:48:32 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -3,6 +3,4 @@ import pandas
 
 streamlit.text('Hello World')
 
-my_fruit_list = pandas.read_json("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=gWioEyTJ4azAEr6PQm4slHxqU2CzzBln9JmoCf93")
-
-streamlit.dataframe(my_fruit_list)
+snowflake-connector-python
