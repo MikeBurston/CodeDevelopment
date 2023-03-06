fbd1d37ca98aa9e561e1038ece317a569c13fcda
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 19:16:18 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -5,12 +5,13 @@ import snowflake.connector
 streamlit.text('Hello World')
 
 cnn = snowflake.connector.connect(
-            user="BIGMIKE2023",
-            password="Army501a!",
-            account="su30921.ca-central-1.aws",
-            warehouse="pc_rivery_wh",
-            database="pc_rivery_db",
-            schema="public")
+            user='BIGMIKE2023',
+            password='Army501a!',
+            account='su30921.ca-central-1.aws',
+            warehouse='pc_rivery_wh',
+            database='pc_rivery_db',
+            schema='public'
+            )
 
 cs = cnn.cursor()
 sql = 'SELECT * FROM FRUIT_LOAD_LIST'
