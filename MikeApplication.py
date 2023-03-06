17be2f56886b0de5e52e3fcacef8528507248884
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 19:54:07 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 streamlit.text('Hello World')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+my_cur = snowflake.connector.connect(**streamlit.secrets["snowflake"]).cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
