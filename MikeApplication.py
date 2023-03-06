a5b35e2ceb982f86dd49d0b461f749d16819ac4c
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 19:09:36 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -1,12 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 
 streamlit.text('Hello World')
 
-conn = snowflake.connector.connect(
-            user="BIGMIKE2023",
-            password="Army501a!",
-            account="su30921.ca-central-1.aws",
-            warehouse="pc_rivery_wh",
-            database="pc_rivery_db",
-            schema="public")
