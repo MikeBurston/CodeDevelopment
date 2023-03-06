92a7e8d147b1d57075f242a4aaaf85690010934e
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 21:46:33 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -1,26 +1,22 @@
-import streamlit
-import requests
-import snowflake.connector
+import snowflake.connector as sf
 import pandas as pd
 
-streamlit.text('Hello World')
-
-cnn = snowflake.connector.connect(
-    user = "BIGMIKE2023",
-    password = "Army501a!",
-    account = "su30921.ca-central-1.aws",
-    warehouse = "pc_rivery_wh",
-    database = "pc_rivery_db",
-    schema = "public",
-    role = "pc_rivery_role"
+ctx = sf.connect(
+    user='BIGMIKE2023',
+    password = 'Army501a!',
+    account = 'su30921.ca-central-1.aws',
+    warehouse = 'compute_wh',
+    database = 'SNOWFLAKE_SAMPLE_DATA',
+    schema = 'INFORMATION_SCHEMA'
     )
 
-cs = cnn.cursor()
-sql = 'Select * From FRUIT_LOAD_LIST'
-cs.execute(sql)
-df = cs.fetch_pandas_all()
+print("Got the context object")
 
+cs = ctx.cursor()
+
+cs.execute("select current_version(), current_user(), current_region()")
+one_row = cs.fetchone()
+print("-------------------")
+print(one_row)
 cs.close()
-cnn.close()
-
-print(df.head(5))
+print("-----All Done------")
