55bf71c8a4e7d3b1bac01bca79914272e74e957d
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-05 19:51:27 -0700

    Update MikeApplication.py

--- MikeApplication.py
+++ MikeApplication.py
@@ -4,11 +4,3 @@ import pandas as pd
 
 streamlit.text('Hello World')
 
-cs = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-sql = "Select * From FRUIT_LOAD_LIST"
-cs.execute(sql)
-df = cs.fetch_pandas_all()
-cs.close()
-cnn.close()
-
-print(df.head(5))
